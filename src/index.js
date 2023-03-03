939a802ce606045a164434766774624053d89442
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-03-02 22:48:33 -0700

    place banner inside div to better style

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import Restaurant from './restaurant.jpg'
 const loadPage = () => {
 const content = document.getElementById('content')
 
+const bannerContent = document.createElement('div');
+
 const myRestaurant = new Image();
 myRestaurant.src = Restaurant
 myRestaurant.setAttribute('id','banner');
 
+bannerContent.appendChild(myRestaurant)
 
-  content.appendChild(myRestaurant);
+  content.appendChild(bannerContent);
   return content
 }
 
