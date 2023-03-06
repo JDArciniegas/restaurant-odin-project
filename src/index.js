5f2ffe5de0e92911c2d18106f8140ad02dbe191a
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-03-06 10:26:39 -0700

    created nav-bar and styled

--- src/index.js
+++ src/index.js
@@ -5,31 +5,36 @@ import contact from "./contact";
 const navBar = () => {
   // create nav ul
   const navBarContainer = document.createElement("ul");
+  const navItemContainer = document.createElement("div");
+  navItemContainer.setAttribute('id', 'nav-container');
+  navBarContainer.appendChild(navItemContainer);
+
   // nav li home
   const homeNav = document.createElement("li");
+  homeNav.classList.add('nav-item');
   homeNav.textContent = "home";
   homeNav.addEventListener("click", () => {
-    content.innerHTML = "";
     content.appendChild(home());
   });
   // nav li menu
   const menuNav = document.createElement("li");
+  menuNav.classList.add('nav-item');
   menuNav.textContent = "menu";
   menuNav.addEventListener("click", () => {
-    content.innerHTML = "";
     content.appendChild(menu());
   });
   // nav li contact
   const contactNav = document.createElement("li");
+  contactNav.classList.add('nav-item');
   contactNav.textContent = "contact";
   contactNav.addEventListener("click", () => {
-    content.innerHTML = "";
     content.appendChild(contact());
   });
 
-  navBarContainer.appendChild(homeNav);
-  navBarContainer.appendChild(menuNav);
-  navBarContainer.appendChild(contactNav);
+  navItemContainer.appendChild(homeNav);
+  navItemContainer.appendChild(menuNav);
+  navItemContainer.appendChild(contactNav);
+  navBarContainer.appendChild(navItemContainer);
   return navBarContainer;
 };
 
